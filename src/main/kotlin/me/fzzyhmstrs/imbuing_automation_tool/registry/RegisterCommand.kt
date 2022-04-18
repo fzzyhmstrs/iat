e227009c2d6588246c263970b871845c3adfc6c6
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-04-17 20:05:25 -0400

    setting up for iat project

---
+++ src/main/kotlin/me/fzzyhmstrs/imbuing_automation_tool/registry/RegisterCommand.kt
@@ -0,0 +1,12 @@
+package me.fzzyhmstrs.imbuing_automation_tool.registry
+
+import com.mojang.brigadier.Command
+import com.mojang.brigadier.context.CommandContext
+
+object RegisterCommand {
+
+    private fun execute(context: CommandContext<Any>): Int{
+        return 1
+    }
+
+}
